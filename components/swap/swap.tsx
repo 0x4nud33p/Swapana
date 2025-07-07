6401a4210dd129afce0370523f73e2a464e63ccd
[anudeep avula] <anudeepavula009@gmail.com>
2025-07-07 18:14:42 +0530

    fix:update

--- components/swap/swap.tsx
+++ components/swap/swap.tsx
@@ -2,32 +2,8 @@
 
 import { Navbar } from "./navbar";
 import { SwapCard } from "./swap-card";
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import { useEffect, useState } from 'react';
 
 export function Swap() {
-  const { connection } = useConnection();
-  const { publicKey } = useWallet();
-  const [network, setNetwork] = useState<string | null>(null);
-
-  useEffect(() => {
-    const checkNetwork = async () => {
-      try {
-        const version = await connection.getVersion();
-        console.log("Cluster version:", version);
-        const slot = await connection.getSlot();
-        const blockTime = await connection.getBlockTime(slot);
-        setNetwork("devnet");
-      } catch (err) {
-        console.error("Error checking network:", err);
-        setNetwork("unknown");
-      }
-    };
-
-    if (publicKey) {
-      checkNetwork();
-    }
-  }, [connection, publicKey]);
   return (
     <div className="min-h-screen relative overflow-hidden transition-all duration-500">
       <div className="absolute inset-0 opacity-20"></div>
@@ -40,12 +16,9 @@ export function Swap() {
         <main className="flex-1 flex items-center justify-center px-4 py-8">
           <SwapCard />
         </main>
-        <footer className="text-center py-6 text-sm text-muted-foreground">
-          <p>This application is for Devnet use only.</p>
-          <span className="font-semibold text-red-300">
-            connected to {network}
-          </span>
-        </footer>
+        {/* <footer className="text-center py-6 text-sm text-muted-foreground">
+          <p></p>
+        </footer> */}
       </div>
     </div>
   );
